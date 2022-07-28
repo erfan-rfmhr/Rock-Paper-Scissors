8126ce0d21bf927357ebe689ad24530d12f77754
Erfan Arefmehr <98986056+erfan-rfmhr@users.noreply.github.com>
2022-07-28 12:52:07 +0430

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,43 +1,106 @@
 import random
+from tkinter import PhotoImage, Tk
+from tkinter.ttk import Label, Button, Style
 
-game = {
+win = Tk()
+win.title('Game')
+win.geometry('750x550')
+win.config(background='black')
+
+obj = {
     'r':'Rock',
     'p':'Paper',
     's':'Scissors'
 }
 
-username = input('Enter your name:')
-
-round = int(input('How many rounds do you play: '))
-
-while round <= 0:
-    round = int(input('You can\'t play 0 or negative round! Try again: '))
-
 user_score = 0
 ai_score = 0
 
-
-while True:
-    print(f'\n{username} score: {user_score}\tComputer score: {ai_score}')      #player will choose between r,p and p
-    user_choice = input('\nChoose between Rock, Paper and Scissors (enter r,p or s):')
-    ai_choice = random.choice(list(game.keys()))    #ai chooses randomly from game dictionary keys
-    print(f'Your choice: {game[user_choice]}\nComputer choice: {game[ai_choice]}')
+def scorebord(user_choice):
+    global user_score, ai_score, obj
+    ai_choice = random.choice(list(obj.keys()))
+    lbl_status['text'] = f'You: {obj[user_choice]}\nAI: {obj[ai_choice]}'
+    lbl_status.grid(row=1, column=4, pady=20)
 
     if (user_choice == 'r' and ai_choice == 's') or (user_choice == 's' and ai_choice == 'p') or (user_choice == 'p' and ai_choice == 'r'):
-        print('You won!')
         user_score += 1
+        lbl_win['text'] = f'Win : {user_score}'
+        lbl_win.grid(row=1, column=2, pady=20)
+        lbl_status['text'] = f'You : {obj[user_choice]}\nAI    : {obj[ai_choice]}\n* You Won *'
+        lbl_status['foreground'] = 'light green'
+        lbl_status.grid(row=1, column=4, pady=20)
     elif user_choice != ai_choice:
-        print('You lost!')
         ai_score += 1
+        lbl_ai['text'] = f'AI : {ai_score}'
+        lbl_ai.grid(row=1, column=7, pady=20)
+        lbl_status['text'] = f'You : {obj[user_choice]}\nAI    : {obj[ai_choice]}\n* You Lost *'
+        lbl_status['foreground'] = 'red'
+        lbl_status.grid(row=1, column=4, pady=20)
     else:
-        print('Draw!')
+        lbl_status['text'] = f'You : {obj[user_choice]}\nAI    : {obj[ai_choice]}\n* Draw *'
+        lbl_status['foreground'] = 'white'
+        lbl_status.grid(row=1, column=4, pady=20)
 
-    if user_score == round // 2 + 1 and ai_score != user_score:
-        print(f'\n************* You won the game {user_score} - {ai_score} *************')
-        break
-    elif ai_score == round // 2 + 1 and ai_score != user_score:
-        print(f'\n************* You lost the game {user_score} - {ai_score} *************')
-        break
-    elif ai_score == round // 2 and user_score == round // 2:
-        print('\n************* Draw *************')
-        break
+
+lbl_title = Label(
+    master=win,
+    text='Rock Paper Scissors',
+    font=('Times new roman', 40),
+    background='black',
+    foreground='white',
+)
+lbl_title.grid(row=0, column=0, columnspan=10, pady=(3, 37))
+
+lbl_win = Label(
+    master=win,
+    text='Win :',
+    font=('Arial', 25),
+    background='black',
+    foreground='white',
+)
+lbl_win.grid(row=1, column=2, pady=20)
+
+lbl_ai = Label(
+    master=win,
+    text='AI :',
+    font=('Arial', 25),
+    foreground='white',
+    background='black',
+)
+lbl_ai.grid(row=1, column=7, pady=20)
+
+lbl_status = Label(
+    master=win,
+    text='Status',
+    font=('Times new roman', 25),
+    foreground='red',
+    background='black',
+)
+lbl_status.grid(row=1, column=4, pady=20)
+
+style = Style()
+style.theme_use('alt')
+style.configure('TButton')
+style.configure('TButton', width = 20, borderwidth=4, focusthickness=0, focuscolor='none',)
+style.map('TButton', background=[('active','green')]) 
+
+img_paper = PhotoImage(
+    file='paper.jpg',
+    height=215,
+)
+Button(master=win, image=img_paper, command=lambda: scorebord('p')).grid(row=4, column=0, rowspan=3, columnspan=3, padx=10)
+
+img_rock = PhotoImage(
+    file='rock.png',
+)
+Button(master=win, image=img_rock, command=lambda: scorebord('r')).grid(row=4, column=3, rowspan=3, columnspan=3, padx=10)
+
+img_scissors = PhotoImage(
+    file='scissors.png',
+    height=215,
+)
+Button(master=win, image=img_scissors, command=lambda: scorebord('s')).grid(row=4, column=7, rowspan=3, columnspan=3, padx=10)
+
+
+
+win.mainloop()
